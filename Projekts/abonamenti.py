aff6b9724695a5c78095ae99b971ad9a3574eea6
Diana10a <144447057+Diana10a@users.noreply.github.com>
2025-04-04 09:14:50 +0300

    Add files via upload

---
+++ Projekts/abonamenti.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+class klienta_informacija():
+    def __init__(self, vards,uzvards,vecums,garums,dzimums):
+        self.vards = vards
+        self.uzvards = uzvards
+        self.vecums = vecums
+        self.garums = garums
+        self.dzimums = dzimums
+    def izvade(self):
+        print(self.vards,self.uzvards,self.vecums,self.garums,self.dzimums)
+vards = input("Ievadiet klienta vārdu: ")
+uzvards = input("Ievadiet klienta uzvārdu: ")
+vecums = input("Ievadiet klienta vecumu: ")
+garums = input("Ievadiet klienta garumu: ")
+dzimums = input("Ievadiet klienta dzimumu: ")
+klients = klienta_informacija(vards,uzvards,vecums,garums,dzimums)
+
+klients.izvade()
+
+class abonamenta_laiki():
+    def __init__(self):
+        abonesanas_laiks = datetime.now()
+        self.abonesanas_laiks = abonesanas_laiks
+        print("Abonamenta izveides laiks", self.abonesanas_laiks.strftime("%Y-%m-%d"))
+        abonesanas_veids = int(input("Izvēlaties abonamentu: "))
+        if abonesanas_veids == 1:
+            self.abonesanas_beigsanas = abonesanas_laiks + timedelta(days=1)
+            print("Abonamenta beigu datums: ", self.abonesanas_beigsanas.strftime("%Y-%m-%d"))
+        if abonesanas_veids == 2:
+            self.abonesanas_beigsanas = abonesanas_laiks + timedelta(days=7)
+            print("Abonamenta beigu datums: ", self.abonesanas_beigsanas.strftime("%Y-%m-%d"))
+        if abonesanas_veids == 3:
+            self.abonesanas_beigsanas = abonesanas_laiks + timedelta(days=30)
+            print("Abonamenta beigu datums: ", self.abonesanas_beigsanas.strftime("%Y-%m-%d"))
+        if abonesanas_veids == 4:
+            self.abonesanas_beigsanas = abonesanas_laiks + timedelta(days=365)
+            print("Abonamenta beigu datums: ", self.abonesanas_beigsanas.strftime("%Y-%m-%d"))
+abonamenta_laiki()
+
+def saglabat_faila(vards,uzvards):
+    fails = vards+"_"+uzvards+".txt"
+    return fails
+
