1551aa57ced2c3c9bffa49eeb8227f2632782beb
Robertsl7 <144447070+Robertsl7@users.noreply.github.com>
2025-04-04 09:30:48 +0300

    drtj
    asks akoskd qp

--- sporta_centra_menedzments.py
+++ sporta_centra_menedzments.py
@@ -10,18 +10,6 @@ def izveleties_abonamentu():
     return 
 
 
-def izveleties_abonamentu():
-    print("Izvēlaties abonamenta veidu: (1- dienas abonaments, 2- nedēļas abonaments, 3- mēneša abonaments, 4- gada abonaments):")
-    izvele1 = input()
-    veidi = {
-        '1': 'dienas',
-        '2': 'nedēļas',
-        '3': 'mēneša',
-        '4': 'gada'
-    }
-    return 
-
-
 
 
 def galvena():
