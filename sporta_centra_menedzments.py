8d29e310987f5b88dacf9967d3e959f903d34e8a
Robertsl7 <144447070+Robertsl7@users.noreply.github.com>
2025-04-08 09:36:46 +0300

    Update sporta_centra_menedzments.py
    awsd

--- sporta_centra_menedzments.py
+++ sporta_centra_menedzments.py
@@ -1,14 +1,14 @@
 import os
 def izveleties_abonamentu():
     print("Izvēlaties abonamenta veidu: (1- dienas abonaments, 2- nedēļas abonaments, 3- mēneša abonaments, 4- gada abonaments):")
-    izvele1 = input()
+    izvele_1 = input()
     veidi = {
         '1': 'dienas',
         '2': 'nedēļas',
         '3': 'mēneša',
         '4': 'gada'
     }
-    return veidi.get(izvele1, "nezināms")
+    return veidi.get(izvele_1, "nezināms")
 
 
 def atcelt_abonamentu():
@@ -26,7 +26,7 @@ def galvena():
         print("Ko jūs vēlaties darīt? (1- izviedot jaunu profilu, 2- dzēst abonamentu, 3- iziet):")
         izvele = input()
         if izvele == '1':
-            klienta_informacija()
+            pass#klienta_informacija() 
         elif izvele == '2':
             atcelt_abonamentu()          
         elif izvele == '3':
